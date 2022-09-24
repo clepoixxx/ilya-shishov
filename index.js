5bc57779d8ca7fe598d2b9b953c6833888dc8e69
clepoixxx <114278942+clepoixxx@users.noreply.github.com>
2022-09-24 09:26:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log ("hello w");
+
+const ourPlanetName = "Земля";
+console.log (ourPlanetName);
+
+const age = 1
+console.log (age)
+
+let kolichestvo = ("12")
+console.log (kolichestvo)
+
+let oplata = true
+console.log (oplata)
+
+let username = ("Ilya") 
+let userf = ("shishov")
+let usero = ("vlad")
+console.log (username + " userf "+ usero) 
+
